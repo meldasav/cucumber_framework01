9fe4279e0178ce3f048bb67bee541a953ac03deb
meldasav <gmeldag83@hotmail.com>
2023-10-02 19:17:51 -0500

    changes added

--- src/test/java/pages/EtsyHomePage.java
+++ src/test/java/pages/EtsyHomePage.java
@@ -11,6 +11,8 @@ public class EtsyHomePage {
     public EtsyHomePage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+    //changes
+    //added
 
     @FindBy(css = "ul[data-ui='top-nav-category-list'] a")
     public List<WebElement> mainNavigationItems;
